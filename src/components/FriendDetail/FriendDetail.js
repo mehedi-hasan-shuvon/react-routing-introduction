11b8542e192f3c6f44272319194dba03471390a0
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-03-31 00:26:32 +0600

    added single friend details

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
     const { friendId } = useParams();
 
+    const [friend, setFriend] = useState({});
+
+    useEffect(() => {
+        const url = `products.json/${friendId}`;
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setFriend(data));
+        console.log(friend);
+    }, []);
     return (
         <div>
             <h2>This is details about dosto: {friendId}</h2>
+
         </div>
     );
 };
